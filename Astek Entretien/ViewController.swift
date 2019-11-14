c999c0fc0962ba6285ec8c80d775a8e26481a10a
Leo <leoguilpain36@gmail.com>
2019-11-14 12:13:03 +0100

    First commit

---
+++ Astek Entretien/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Astek Entretien
+//
+//  Created by Léo Guilpain on 14/11/2019.
+//  Copyright © 2019 Astek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
